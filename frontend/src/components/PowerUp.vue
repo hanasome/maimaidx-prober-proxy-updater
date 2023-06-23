24b08f5be0842af9b59d268a001d269d228060bb
hanasome <64626847+hanasome@users.noreply.github.com>
2023-06-23 13:50:03 +0800

    Update PowerUp.vue

--- frontend/src/components/PowerUp.vue
+++ frontend/src/components/PowerUp.vue
@@ -1,7 +1,9 @@
 <template>
   <n-card :bordered="false" title="在线乞讨">
     请 Bakapiano 打一局卖卖 ~~
-    <a href="https://afdian.net/a/bakapiano">发个电</a>
+    <a href="https://afdian.net/a/bakapiano">发个电</a>\
+    </br>
+    <a href="https://mcake.space/help.html">使用教程</a>
   </n-card>
 </template>
 
